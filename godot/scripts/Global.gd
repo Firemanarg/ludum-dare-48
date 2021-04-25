3b925ea856fe3be26cd5e687eba0b66b2486b34a
Lanysc <36964927+Lanysc@users.noreply.github.com>
2021-04-25 10:52:04 -0300

    Enemy

--- godot/scripts/Global.gd
+++ godot/scripts/Global.gd
@@ -3,7 +3,7 @@ extends Node
 var nav_2d : Navigation2D = null
 var Enemy : KinematicBody2D = null
 var player : KinematicBody2D = null
-var _plaayerLife : int = 1
+var _playerLife : int = 0
 
 func map(value, low1, high1, low2, high2):
 	return low2 + (value - low1) * (high2 - low2) / (high1 - low1)
--- godot/scripts/level/Enemy.gd
+++ godot/scripts/level/Enemy.gd
@@ -27,7 +27,7 @@ func _physics_process(delta: float) -> void:
 	move_along_path(move_distance)
 	near_attack()
 	
-	print(timer.time_left)
+#	print(timer.time_left)
 	# I need to fix the code bellow and make a func, this timer only works one time ************
 #	if self.transform.origin == last_seen:
 #		if seek == false:
@@ -39,8 +39,6 @@ func _physics_process(delta: float) -> void:
 #			if timeout == true:
 #				timer.stop()
 #				seek == false
-#				speed = 150.0
-#				go_in_a_place(starting_position)
 
 func move_along_path(distance : float) -> void:
 	var last_point : = position
@@ -75,8 +73,18 @@ func go_in_a_place(where) -> void:
 
 func _on_Timer_timeout() -> void:
 	timeout = true
+	speed = 150.0
+	go_in_a_place(starting_position)
 
 func near_attack() -> void:
-	if self.transform.origin.distance_to(Global.player.transform.origin) < 100:
+	if self.transform.origin.distance_to(Global.player.transform.origin) < 800 && Global._playerLife > 0:
 		speed = 250.0
 		go_in_a_place(Global.player.transform.origin)
+
+
+func _on_Area2D_body_entered(body: Node) -> void:
+	print("asdyhvasdguaysdb     " + body.name)
+	if not body == self.get_node("Area2D"):
+		Global._playerLife -= 1
+		if(Global._playerLife <= 0):
+			print("You died !!!")
