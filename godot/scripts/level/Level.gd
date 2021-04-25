7e8da1924cb6aa2c28a74a90748ec233ad50d576
Lanysc <wic18dark@gmail.com>
2021-04-25 18:24:47 -0300

    sign

--- godot/scripts/level/Level.gd
+++ godot/scripts/level/Level.gd
@@ -1,8 +1,13 @@
 extends Node
 
+onready var custom_sign = get_node("Area2D")
+
 func _ready() -> void:
 	Global.nav_2d = get_node("Navigation2D")
 	Global.player = get_node("Player")
 	Global.Enemy = get_node("Enemy")
 	Global._playerLife = 1
+	custom_sign.connect("interacted", self, "show_textbox")
 
+func show_textbox () -> void:
+	$CanvasLayer/Control.visible = true
