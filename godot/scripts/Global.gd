69c5e612bad92c096b86271a3ce5134c71cc30c1
Lanysc <36964927+Lanysc@users.noreply.github.com>
2021-04-25 09:15:36 -0300

    Enemy

--- godot/scripts/Global.gd
+++ godot/scripts/Global.gd
@@ -1,6 +1,9 @@
 extends Node
 
+var nav_2d : Navigation2D = null
+var Enemy : KinematicBody2D = null
+var player : KinematicBody2D = null
+var _plaayerLife : int = 1
 
 func map(value, low1, high1, low2, high2):
 	return low2 + (value - low1) * (high2 - low2) / (high1 - low1)
-
--- godot/scripts/level/LightSource.gd
+++ godot/scripts/level/LightSource.gd
@@ -5,15 +5,11 @@ onready var gradient = get_node("Gradient")
 
 var min_radius = 64
 var max_radius = 512
-var last_prc = 0.6
-export var min_scale = 0.5
-export var max_scale = 1.5
-export(float, 0.0, 1.0) var current_prc = 0.6
+var current_prc = 0.6
 var prc_step = 0.2
 
 
 func _ready() -> void:
-	update_visual_radius()
 	pass
 
 
@@ -38,15 +34,10 @@ func _process(delta: float) -> void:
 	if light_off:
 		current_prc = 1.0
 
-	if last_prc != current_prc:
-		update_visual_radius()
-
 
 func current_radius():
 	return Global.map(current_prc, 0.0, 1.0, min_radius, max_radius)
 
 
 func update_visual_radius():
-	var texture_scale = Global.map(current_radius(), min_radius, max_radius, min_scale, max_scale)
-	gradient.scale = Vector2(texture_scale, texture_scale)
 
