5afd7e5f421ec46079f864cfe1e2f822078e33c3
Firemanarg <firemanarg@gmail.com>
2021-05-01 17:04:06 -0300

    Removed RadiusLine

--- godot/scripts/Globals/GlobalLoaded.gd
+++ godot/scripts/Globals/GlobalLoaded.gd
@@ -4,14 +4,16 @@ var load_queue = []		# [ {"name": "name", "path": "path"} ]
 
 var loaded: Dictionary = {}
 
+var load_in_progress = false
+
 func _ready() -> void:
 	pass
 
-#func add_to_load_queue
+func add_to_load_queue(_name: String, _path: String):
+	if not load_in_progress:
+		load_queue.append( {"name": _name, "path": _path} )
 
-func _request_load(_name: String, _path: String):
-	if not _name in loaded.keys():
-		loaded[_name] = load(_path)
+func start_load():
 	pass
 
 func _is_fully_loaded() -> bool:
