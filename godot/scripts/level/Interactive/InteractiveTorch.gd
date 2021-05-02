52a64579d53352e6252846cddefaa76220419399
Lanysc <wic18dark@gmail.com>
2021-05-02 01:55:23 -0300

    life

--- godot/scripts/level/Interactive/InteractiveTorch.gd
+++ godot/scripts/level/Interactive/InteractiveTorch.gd
@@ -1,4 +1,14 @@
 extends BaseInteractive
 
+func _on_Area2D_body_entered(body: Node) -> void:
+	if body is Player:
+		can_interact = true
+	elif body is Enemy:
+		enemyInteract()
+
 func interact() ->void:
 	print("Torch")
+
+func enemyInteract() -> void:
+	#if torch is on
+	print("Heheheehehehehe")
