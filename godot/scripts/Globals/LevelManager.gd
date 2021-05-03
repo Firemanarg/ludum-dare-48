67ce32ebdcb1579e50f255f4bdf72ae53e26c92f
Firemanarg <firemanarg@gmail.com>
2021-05-02 21:50:53 -0300

    Update LevelManager.gd

--- godot/scripts/Globals/LevelManager.gd
+++ godot/scripts/Globals/LevelManager.gd
@@ -1,7 +1,7 @@
 extends Node
 
 var nav_2d : Navigation2D = null
-var Enemy : Enemy = null
+var enemy : Enemy = null
 var player : Player = null
 var _playerLife : int = 0
 var picked_up_the_key = false
@@ -12,11 +12,11 @@ var current_level = null
 
 var is_game_active = true
 
-func map(value, low1, high1, low2, high2):
-	return low2 + (value - low1) * (high2 - low2) / (high1 - low1)
-
 func is_game_active():
 	return is_game_active
+
+func set_game_active(state: bool):
+	is_game_active = state
 
 func show_dialog_box(dialog):
 	if current_level:
@@ -25,3 +25,18 @@ func show_dialog_box(dialog):
 func hide_dialog_box():
 	if current_level:
 		current_level.hide_dialog_box()
+
+func get_player_life() -> int:
+	return _playerLife
+
+func set_player_life(value: int) -> void:
+	_playerLife = value
+
+func is_key_picked_up() -> bool:
+	return picked_up_the_key
+
+func pickup_key():
+	picked_up_the_key = true
+
+func reset_key_pickup():
+	picked_up_the_key = false
